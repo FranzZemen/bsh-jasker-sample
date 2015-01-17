36b062bbc551a95e279cf1dd1359c447e1d04443
FranzZemen <FranzZemen@gmail.com>
2015-01-17 16:15:15 -0600

    Trivial Flow

--- app/jaskerSample.js
+++ app/jaskerSample.js
@@ -5,11 +5,74 @@
 (function () {
     'use strict';
 
-    var log = require ('bunyan').createLogger({name:'jaskerSample', level : 'debug'});
-    var jasker = require ('bsh-jasker');
+    var log = require('bunyan').createLogger({name: 'jaskerSample', level: 'debug'});
+    var when = require('node-promise').when;
+    var all = require('node-promise').all;
+    var jasker = require('bsh-jasker');
 
-    module.exports.jaskerSample = function () {
-        var jaskerMap = new jasker.JaskerMap();
-        jaskerMap.initilize({inline : {}});
+    var jaskerMap = new jasker.JaskerMap();
+    var jaskerInstance;
+
+    var stateEnum = {
+        sample1: 'sample1',
+        sample2: 'sample2',
+        sample3: 'sample3'
+    };
+
+    jaskerMap.initialize(
+        {
+            inline: {
+                name: 'jaskerSample',
+                states: {
+                    sample1: {
+                        next: stateEnum.sample2
+                    },
+                    sample2: {
+                        next: stateEnum.sample3
+                    },
+                    sample3: {
+                        next: stateEnum.sample1
+                    }
+                }
+            }
+        }).then(
+        function (val) {
+            log.info('Successfully initialized jaskerMap');
+
+            var document = {
+                field1: 'someValue'
+            };
+            jaskerInstance = new jasker.JaskerInstance(jaskerMap, stateEnum.sample1, document);
+            logCurrentState('Starting flow');
+            setTimeout(function () {
+                start().then(function (val) {
+                    log.info('Done!');
+                }, function (err) {
+                    log.error(err);
+                });
+            }, 500);
+
+        }, function (err) {
+            log.error(err);
+        }
+    );
+    function start() {
+        log.info('Starting to step');
+        return when(jaskerInstance.next(), function () {
+            logCurrentState('Stepped once');
+            return when(jaskerInstance.next(), function () {
+                logCurrentState('Stepped twice');
+                return when(jaskerInstance.next(), function () {
+                    logCurrentState('Stepped thrice');
+                    log.info('Completed stepping');
+                    setTimeout(function () {
+                    }, 1000);
+                });
+            });
+        });
+    }
+
+    function logCurrentState(prefix) {
+        log.info(prefix + '. Current state is %s', jaskerInstance.current());
     }
 })();
